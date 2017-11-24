21efb128fecb51ed35da43fd21a9fe5ebb42f07d
DESKTOP-PLLREIU\Reggie <reggiecril0618@gmail.com>
2017-11-24 19:04:44 +0000

    Created a sample login(for tester).Next: create a login for developer.

--- Bug_Tracker/Form1.cs
+++ Bug_Tracker/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Bug_Tracker
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Reggie\Documents\Bug_Tracker_Table.mdf;Integrated Security=True;Connect Timeout=30");
+            SqlDataAdapter da = new SqlDataAdapter("Select Count(*) From [user] where EMAIL = '" + textBox1.Text + "' and PASSWORD= '" + textBox2.Text + "'", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows[0][0].ToString() == "1")
+            {
+                this.Hide();
+                main m = new main();
+                m.Show();
+            }
+            else
+            {
+                MessageBox.Show("Please Check Your e-mail or password");
+            }
+
+           
         }
     }
 }
